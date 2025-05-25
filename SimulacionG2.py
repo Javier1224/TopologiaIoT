ce8427f3fada09a1f92a9842c73e9d8bccdeec05
Javier1224 <jajole02@hotmail.com>
2025-05-25 09:19:01 -0700

    SImilaciÃiopara pruebas

--- SimulacionG2.py
+++ SimulacionG2.py
@@ -20,17 +20,17 @@ def startNetwork():
     # Se crean los hosts 
     net.addHost(Dispositivos[i],ip="10.0.0."+str(i+1)+"/24")
     # Se vinculan los host al switch con los parametros predefinidos
-    net.addLink(Dispositivos[i],"S1", bw=100, delay='5ms', loss=0.01) # 5G
-     
+    #net.addLink(Dispositivos[i],"S1", bw=100, delay='5ms', loss=0.01) # 5G
+    net.addLink(Dispositivos[i],"S1", bw=100, delay='5ms', loss=0.01)
   net.start()
   
+  #["stnMlogica","50K","250K","u"],
   PruebasPing = [
-  ["CamMovi","50M","8192K","u"],
-  ["SensAud","300","800","u"],
-  ["Velocimet","10M","16K","t"],
-  ["stnMlogica","50K","250K","u"],
-  ["SenPeso","5M","80K","t"],
-  ["CamSeg","150M","8192K","u"]]
+  ["CamMovi","50M","8192K","u","3"],
+  ["SensAud","300","800","u","1"],
+  ["Velocimet","10M","16K","t","2"],
+  ["SenPeso","5M","80K","t","1"],
+  ["CamSeg","150M","8192K","u","1"]]
 
   IP_dest = str(net.get("PtoRecol").IP())
   option=0
@@ -60,13 +60,17 @@ def startNetwork():
       option = 0
     #===================================================
     if option == 5:
-      net.get("PtoRecol").cmd("iperf -s > /dev/null 2>&1 &") # Escucha el servidor
+      net.get("PtoRecol").cmd("iperf -s > /dev/null 2>&1 & iperf -s -u -p 5002 > /dev/null 2>&1 &") # Escucha el servidor con un puerto TCP y otro en UDP
+      #net.get("PtoRecol").cmd("iperf -s -u -p 5002> /dev/null 2>&1 &")
+      
       for host in range(len(PruebasPing)):
         if PruebasPing[host][3] == "u":# Simula dispositivos UDP
-          client_output = net.get(PruebasPing[host][0]).cmd("iperf -c " + IP_dest + " -n "+PruebasPing[host][1]+" -b "+PruebasPing[host][2])  # Cliente con sus parametros
+          print("Prueba del sensor: ",PruebasPing[host][0],"##########################################################################################")
+          client_output = net.get(PruebasPing[host][0]).cmd("iperf -u -c " + IP_dest + " -n "+PruebasPing[host][1]+" -b "+PruebasPing[host][2]+" -p 5002 > /tmp/h1.txt &")  # Cliente con sus parametros
           print(client_output)
         else:# Simula dispositivos TCP
-          client_output = net.get(PruebasPing[host][0]).cmd("iperf -c " + IP_dest + " -n "+PruebasPing[host][1]+" -l "+PruebasPing[host][2])  # Cliente con sus parametros
+          print("Prueba del sensor: ",PruebasPing[host][0],"##########################################################################################")
+          client_output = net.get(PruebasPing[host][0]).cmd("iperf -c " + IP_dest + " -n "+PruebasPing[host][1]+" -l "+PruebasPing[host][2]," -i 1")  # Cliente con sus parametros
           print(client_output)
       option = 0
     #===================================================
